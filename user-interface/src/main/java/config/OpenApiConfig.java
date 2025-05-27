618bdb0bc080106bcc6f5fd10d6dc992077a5f13
Juan Segundo Arnaude <juansarnaude@gmail.com>
2025-05-26 23:07:40 -0300

    FIx postman

--- user-interface/src/main/java/config/OpenApiConfig.java
+++ user-interface/src/main/java/config/OpenApiConfig.java
@@ -2,8 +2,8 @@ package config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,17 +11,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "ApiKeyAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
-                        .addSecuritySchemes("Api Key",
-                                new SecurityScheme().type(SecurityScheme.Type.APIKEY).name("x-api-key").in(SecurityScheme.In.HEADER)))
-                .info(
-                        new Info()
-                                .title("Recipe Service").version("0.0.1")
-                                .description("Simple REST Api which allows users to manage their favourite recipes")
-                                .contact(new Contact().name("Raul Parola").email("parolaraul@gmail.com"))
-                );
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .name("x-api-key")
+                                        .in(SecurityScheme.In.HEADER)))
+                .info(new Info()
+                        .title("Recipe Service")
+                        .version("0.0.1")
+                        .description("Simple REST Api which allows users to manage their favourite recipes"));
     }
 }
